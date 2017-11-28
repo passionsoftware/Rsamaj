fac4ddd0d60b2f97f963d71805951b0488952b32
passionsoftware <passion.software2013@gmail.com>
2017-11-28 13:24:18 +0530

    Full chnages

---
+++ RS.BusinessLogicLayer/RathoreDetailBL.cs
@@ -0,0 +1,55 @@
+﻿using RS.DataAccessLayer;
+using RS.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RS.BusinessLogicLayer
+{
+    public class RathoreDetailBL
+    {
+        RathoreDetailDL _rathoreDetailDL;
+
+        public RathoreDetailBL()
+        {
+            _rathoreDetailDL = new RathoreDetailDL();
+        }
+
+        public int InsertRathoreDetail(RathoreDetailModel objRathoreDetail)
+        {
+            int result = 0;
+            result = _rathoreDetailDL.InsertRathoreDetail(objRathoreDetail);
+            return result;
+        }
+
+        public int UpdateRathoreDetail(RathoreDetailModel objRathoreDetail)
+        {
+            int result = 0;
+            result = _rathoreDetailDL.UpdateRathoreDetail(objRathoreDetail);
+            return result;
+        }
+
+        public int DeleteRathoreDetail(int rathoreDetailId)
+        {
+            int result = 0;
+            result = _rathoreDetailDL.DeleteRathoreDetail(rathoreDetailId);
+            return result;
+        }
+
+        public List<RathoreDetailModel> GetAllRathoreDetail()
+        {
+            List<RathoreDetailModel> result = new List<RathoreDetailModel>();
+            result = _rathoreDetailDL.GetAllRathoreDetail();
+            return result;
+        }
+
+        public RathoreDetailModel GetRathoreDetailId(int rathoreDetailId)
+        {
+            RathoreDetailModel result = new RathoreDetailModel();
+            result = _rathoreDetailDL.GetRathoreDetailId(rathoreDetailId);
+            return result;
+        }
+    }
+}
--- RS.DataAccessLayer/RathoreDetailDL.cs
+++ RS.DataAccessLayer/RathoreDetailDL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RS.DataModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,38 @@ namespace RS.DataAccessLayer
 {
     public class RathoreDetailDL
     {
-        public int InsertRathoreDetail()
+        public int InsertRathoreDetail(RathoreDetailModel objRathoreDetail)
         {
             int result = 0;
+
+            return result;
+        }
+
+        public int UpdateRathoreDetail(RathoreDetailModel objRathoreDetail)
+        {
+            int result = 0;
+
+            return result;
+        }
+
+        public int DeleteRathoreDetail(int rathoreDetailId)
+        {
+            int result = 0;
+
+            return result;
+        }
+
+        public List<RathoreDetailModel> GetAllRathoreDetail()
+        {
+            List<RathoreDetailModel> result = new List<RathoreDetailModel>();
+
+            return result;
+        }
+
+        public RathoreDetailModel GetRathoreDetailId(int rathoreDetailId)
+        {
+            RathoreDetailModel result = new RathoreDetailModel();
+
 
             return result;
         }
