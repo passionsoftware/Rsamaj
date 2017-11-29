7e066c899cb387334d64b84e8c17aa23ab611025
passionsoftware <passion.software2013@gmail.com>
2017-11-29 13:11:14 +0530

    data annotation

--- RS.DataModel/ContactModel.cs
+++ RS.DataModel/ContactModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +12,19 @@ namespace RS.DataModel
 
         public int ContactusId { get; set; }
 
+
+        [Required(ErrorMessage ="Name is required!!")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Invalid Name!!")]
         public string Name { get; set; }
 
+        [StringLength(10, MinimumLength = 10,ErrorMessage = "Invalid Phone number!!")]
+        [Required(ErrorMessage = "Phone Number is required!!")]
         public string PhoneNumber { get; set; }
 
+        [EmailAddress(ErrorMessage = "Invalid Email Address!!")]
         public string EmailId { get; set; }
 
+        [Required(ErrorMessage = "Subject is required!!")]
         public string Subject { get; set; }
 
         public string Body { get; set; }
