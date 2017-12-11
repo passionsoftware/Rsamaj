750b5f0fb37e246aaf9eeaf20469054ad788e70e
passionsoftware <passion.software2013@gmail.com>
2017-12-11 12:13:25 +0530

    event and text editor

--- RS.DataModel/EventModel.cs
+++ RS.DataModel/EventModel.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-
+using System.Web.Mvc;
 
 namespace RS.DataModel
 {
@@ -13,30 +13,47 @@ namespace RS.DataModel
     {
         public int EventId { get; set; }
 
+        
         public int EventType { get; set; }
 
+        [Required(ErrorMessage = "जानकारी अनिवार्य है !!")]
+        [StringLength(15, MinimumLength = 3, ErrorMessage = "काम से काम 3 - 15 शब्दों में जानकारी भरे !!")]
         public string EventTypeName { get; set; }
 
+        [Required(ErrorMessage = "जानकारी अनिवार्य है !!")]
+        [StringLength(100, MinimumLength = 4, ErrorMessage = "काम से काम 4 - 100 शब्दों में जानकारी भरे !!")]
         public string Subject { get; set; }
 
-       
+        [Required(ErrorMessage = "जानकारी अनिवार्य है !!")]
+        [StringLength(300, MinimumLength = 10, ErrorMessage = "काम से काम 4 - 300 शब्दों में जानकारी भरे !!")]
+        [AllowHtml]
         public string Body { get; set; }
 
+        [Required(ErrorMessage = "जानकारी अनिवार्य है !!")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "काम से काम 10 शब्दों में जानकारी भरे !!")]
         public DateTime? EventDateFrom { get; set; }
 
+        [Required(ErrorMessage = "जानकारी अनिवार्य है !!")]
         public string EventTimeFrom { get; set; }
 
+        [Required(ErrorMessage = "जानकारी अनिवार्य है !!")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "काम से काम 10 शब्दों में जानकारी भरे !!")]
         public DateTime? EventDateTo { get; set; }
 
+        [Required(ErrorMessage = "जानकारी अनिवार्य है !!")]
         public string EventTimeTo { get; set; }
 
         public int? Location { get; set; }
 
+        [Required(ErrorMessage = "जानकारी अनिवार्य है !!")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "काम से काम 3 -20 शब्दों में जानकारी भरे !!")]
         public string LocationName { get; set; }
 
 
         public int? District { get; set; }
 
+        [Required(ErrorMessage = "जानकारी अनिवार्य है !!")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "काम से काम 3 -20 शब्दों में जानकारी भरे !!")]
         public string DistrictName { get; set; }
 
 
